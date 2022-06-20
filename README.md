838b3450acf23109f371e3821115c18a82450e37
Subash Poudel <subashpoudel@lftechnology.com>
2022-06-20 09:31:10 +0545

    master: Add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Typescript starter pack
+Small starter pack pre-configured for trying out typescript.
+Not for production use.
+
+
+## Usage
+```
+npm run dev
+// should output "Starter pack working."
+```
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console, @typescript-eslint/no-explicit-any */
 function start() {
-   console.log('start function called and tested');
+   console.log('Starter pack working.');
 }
 
 start();
